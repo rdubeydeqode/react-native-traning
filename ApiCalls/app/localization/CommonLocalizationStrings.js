4a67bd5118ef3aeb294e80c886b81baad12b9d1b
Rahul <rdubey@deqode.com>
2021-02-24 17:54:08 +0530

    API call using fetch

---
+++ ApiCalls/app/localization/CommonLocalizationStrings.js
@@ -0,0 +1,9 @@
+import LocalizedStrings from 'react-native-localization';
+
+export const CommonLocalizeStrings = new LocalizedStrings({
+  // English
+  en: {
+    go_to_axios_call: 'Go to Axios Calls',
+    get_call: 'Get Call',
+  },
+});
